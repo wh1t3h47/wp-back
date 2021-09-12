cb1fc1e916e421b20ad972a01ffa1948cd1d4c9e
wh1t3h47 <tom.mharres@gmail.com>
2021-09-12 17:45:52 -0300

    Nova estrutura de plugin :metal:

---
+++ plugin.php
@@ -0,0 +1,18 @@
+<?php
+echo("hi wordpress plugin");
+/*When assigning a version number to your project, keep in mind that WordPress uses the PHP version_compare() function to compare plugin version numbers. Therefore, before you release a new version of your plugin, you should make sure that this PHP function considers the new version to be “greater” than the old one. For example, 1.02 is actually greater than 1.1.*/
+/**
+ * Plugin Name:       Plugin ecommerce wordpress
+ * Plugin URI:        https://github.com/wh1t3h47
+ * Description:       Plugin de ecommerce wordpress
+ * Version:           0.0.1
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            wh1t3 h47
+ * Author URI:        https://author.example.com/
+ * License:           wh1t3h47 software license ©®(./*)
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+?>
+
