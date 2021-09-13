00a9e43fa16b356ba40c7fb536123a7ac3df2527
wh1t3h47 <tom.mharres@gmail.com>
2021-09-13 09:39:24 -0300

    conflito

--- plugin.php
+++ plugin.php
@@ -1,9 +1,5 @@
 <?php
-<<<<<<< HEAD
-echo("hi wordpress plugin");
-=======
 echo ("hi wordpress plugin");
->>>>>>> 8875741005c6c445033c76e3e7317958e8a1099b
 /*When assigning a version number to your project, keep in mind that WordPress uses the PHP version_compare() function to compare plugin version numbers. Therefore, before you release a new version of your plugin, you should make sure that this PHP function considers the new version to be “greater” than the old one. For example, 1.02 is actually greater than 1.1.*/
 /**
  * Plugin Name:       Plugin ecommerce wordpress
@@ -13,17 +9,7 @@ echo ("hi wordpress plugin");
  * Requires at least: 5.2
  * Requires PHP:      7.2
  * Author:            wh1t3 h47
-<<<<<<< HEAD
- * Author URI:        https://author.example.com/
- * License:           wh1t3h47 software license ©®(./*)
- * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- */
-
-?>
-
-=======
  * Author URI:        https://github.com/wh1t3h47
  * License:           wh1t3h47 software license ®©®©®©®©®©®©®©®©®©®©®
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  */
->>>>>>> 8875741005c6c445033c76e3e7317958e8a1099b
